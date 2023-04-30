a1bb167aba00095c3414ed1aee7ee0cb38992779
Raghad <rsed84@gmail.com>
2023-04-30 03:33:38 +0300

    Updates

---
+++ src/Booking.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import BookingForm from "./BookingPage";
+
+const Booking = () => {
+  const navigate = useNavigate();
+
+  return (
+    <main className="booking">
+      <h1>Booking</h1>
+      <section>
+        <BookingForm navigate={navigate} />
+      </section>
+    </main>
+  );
+};
+
+export default Booking;
